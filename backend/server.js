8d4f5bf2688e4c209791121b6c374e6621732f2c
danbryanleoncito <danbryanleoncito@gmail.com>
2025-01-31 03:35:15 +0800

    added backend mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,24 @@ import express from "express"
 import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.routes.js"
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+dotenv.config();
 
-app.get("/", (req, res) => {
-    //root route http://localhost:5000/
-    res.send("Hello World!")
-})
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.get("/", (req, res) => {
+//     //root route http://localhost:5000/
+//     res.send("Hello World!")
+// })
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`)
+});
 
